fb26cf0b4cc7e39888fca40d79f4f3bf3a84843a
test-lexion <hyamamoto334@gmail.com>
2025-10-09 02:37:57 +0100

    Update Futures.tsx

--- src/pages/Futures.tsx
+++ src/pages/Futures.tsx
@@ -5,10 +5,12 @@ import { PositionsTable } from "@/components/trading/PositionsTable";
 
 const Futures = () => {
   return (
-    <div className="flex h-[calc(100vh-4rem)] flex-col">
+    <div className="flex h-full flex-col"> {/* Use h-full to fill the main content area */}
       <div className="flex flex-1 overflow-hidden">
-        {/* Markets Sidebar */}
-        <MarketsSidebar />
+        {/* Hide sidebar on mobile for better usability */}
+        <div className="hidden shrink-0 md:flex">
+          <MarketsSidebar />
+        </div>
 
         {/* Main Trading Area */}
         <div className="flex flex-1 flex-col gap-4 overflow-y-auto p-4">
